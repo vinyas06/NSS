c3a3d6b40e9e652b80170828d38ef51ad76cfdfa
vinyas06 <vinyassharana06@gmail.com>
2026-09-10 22:30:11 +0530

    Add Vercel configuration files api/index.py and vercel.json

---
+++ api/index.py
@@ -0,0 +1,12 @@
+import sys
+import os
+
+root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+backend_dir = os.path.join(root_dir, "backend")
+
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+if backend_dir not in sys.path:
+    sys.path.insert(0, backend_dir)
+
+from backend.server import app
